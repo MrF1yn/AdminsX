3f6ca6e541c792a98850e2c5c9884d96325e8010
Dev-akmfop <86141373+MrF1yn@users.noreply.github.com>
2022-08-10 17:25:54 +0530

    InitialCommit

---
+++ src/main/java/xyz/vectlabs/adminsx/AdminsX.java
@@ -0,0 +1,17 @@
+package xyz.vectlabs.adminsx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminsX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+            
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
